b7f1d06e7f8a60f4a88635383ad26cd30e557a47
asen23 <andersenlim23@gmail.com>
2022-05-28 17:34:04 +0700

    add injection class

---
+++ src/di/Injection.java
@@ -0,0 +1,20 @@
+package di;
+
+import db.ITagRepository;
+import db.ITodoRepository;
+import db.mysql.TagDatabase;
+import db.mysql.TodoDatabase;
+import db.repository.TagRepository;
+import db.repository.TodoRepository;
+
+public class Injection {
+    public static ITodoRepository getTodoRepository() {
+        ITodoRepository db = new TodoDatabase();
+        return new TodoRepository(db);
+    }
+
+    public static ITagRepository getTagRepository() {
+        ITagRepository db = new TagDatabase();
+        return new TagRepository(db);
+    }
+}
