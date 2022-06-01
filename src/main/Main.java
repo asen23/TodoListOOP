c91f4a2032b13b41bb57dbf6e0f72df9d3a01346
Denzel.wendy@binus.ac.id <denzel.wendy@binus.ac.id>
2022-06-01 17:05:17 +0700

    Add Menu Class, and print functions

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
+import menu.Menu;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Menu menu = new Menu();
     }
 }
