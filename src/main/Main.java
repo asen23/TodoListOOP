0863a51dc01d65a53bbf56d7a8c89a8586759f7f
Denzel.wendy@binus.ac.id <denzel.wendy@binus.ac.id>
2022-06-02 13:25:45 +0700

    Remove unused code in Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,8 @@
 package main;
 
-import menu.Menu;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Menu menu = new Menu();
     }
 }
