f26d1a64b23402daf50681c2ebc67d0faae6c778
Winsta <75075355+MikazukiMizuki@users.noreply.github.com>
2022-06-11 21:03:53 +0700

    Update ITodoRepository.java

--- src/db/ITodoRepository.java
+++ src/db/ITodoRepository.java
@@ -7,22 +7,13 @@ import java.util.List;
 
 public interface ITodoRepository {
 	void addTodo(String title, String description);
-
 	boolean updateTodo(int id, String title, String description);
-
 	boolean updateTodo(int id, boolean isDone);
-
 	boolean deleteTodo(int id);
-
 	boolean addTag(int id, int tagId);
-
 	boolean removeTag(int id, int tagId);
-
 	Todo getTodoById(int id);
-
 	List<Todo> getTodo();
-
 	List<Todo> getTodoByTitleQuery(String query);
-
 	List<Todo> getTodoByTagId(int id);
 }
